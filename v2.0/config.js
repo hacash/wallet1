187fc2eb26992d6e6a30ec4969bfef6be25dfada
jojoin <yangjie@jojoin.com>
2022-06-04 15:25:03 +0800

    up

--- v2.0/config.js
+++ v2.0/config.js
@@ -4,6 +4,6 @@ module.exports = {
     http_port: 8009,
     watch_restart_timeout: 0,
     
-    hcx_node_api_url: 'http://node2.hacash.org:3338',
+    hcx_node_api_url: 'http://127.0.0.1:3338',
 
 }
