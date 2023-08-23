e5de30faab62787ae5cd7a127ae2ea1f97e5bdba
hacashcom <ken@hacash.com>
2023-08-23 11:45:08 +0800

    up

--- v2.0/language/en_US/index.js
+++ v2.0/language/en_US/index.js
@@ -4,6 +4,15 @@ module.exports = {
 
     pttl: "Wallet Tools",
 
+    wlts: {
+        create: ["一键生成地址","点击立即创建新地址，用来接收HAC和HACD", "网页，无需下载，支持所有设备", "立即创建"],
+        online: ["在线网页钱包","打开网页就可进行HAC和HACD转账，随机或按密码创建地址","网页，无需下载，支持所有设备","立即使用"],
+        offline: ["离线网页钱包","更加安全的网页钱包，可以离线操作","网页，需下载，支持所有设备","下载"],
+        desktop: ["桌面钱包","含有最全面的功能，为追求安全的资深用户准备","需下载W，支持Windows/Ubuntu/MacOS","下载"],
+        cmds: ["命令行钱包","开发者专用，命令行操作界面","需自己编译，支持所有电脑系统","Github"],
+        miner: ["矿工节点钱包","搭建全节点连接网页钱包，可转账或进行HACD手动竞价","网页，需下载，支持所有设备","下载"],
+    },
+
     send: [
         "Send Transaction to Blockchain",
         `Paste the TxBody value of the transaction into the box below, and click confirm to submit it to the blockchain network for confirmation.`,
