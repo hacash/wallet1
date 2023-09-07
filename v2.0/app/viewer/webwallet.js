3e918d4f88bb1de7343d9fd725f4612624404397
jojoin <yangjie@jojoin.com>
2023-09-08 00:32:42 +0800

    + BTC transfer

--- v2.0/app/viewer/webwallet.js
+++ v2.0/app/viewer/webwallet.js
@@ -4,7 +4,7 @@ exports.components = [
     'html',
     'header',
 
-    'webframe',
+    'web',
 
     'footer',
     'tail',
@@ -14,10 +14,20 @@ exports.components = [
 exports.datas = async function(cnf, ctx)
 {
     // let data_types = await ddd.getTypes()
-    
+    // console.log(ctx.req, ctx.query)
+    var q = ctx.query || {}
+    var chain_id = undefined
+    , chain_name = ''
+    if(q.chain_id > 0) {
+        chain_id = q.chain_id
+        chain_name = q.chain_name
+    }
+
     return {
         // isMobile: types.isMobile(ctx.req),
         // numToThousands: utilnumber.numToThousands,
+        chain_id: chain_id,
+        chain_name: chain_name,
         title: "Web Wallet"
     }
 }
